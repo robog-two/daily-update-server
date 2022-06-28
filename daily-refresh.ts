5bd033e38c5989c69781841d2367b10afad74baf
Sam Knight <robogamingbysam@icloud.com>
2022-06-28 16:09:30 -0500

    Finish mvp

---
+++ daily-refresh.ts
@@ -0,0 +1,77 @@
+import { connect as getMongo } from './mongo.ts'
+import { isProd } from './isprod.ts'
+
+let lastStart = 0
+let queue: Array<string | undefined> = []
+const mongo = await getMongo()
+const products = await mongo.database('wishlily').collection('products')
+
+interface Embed {
+  link: string | undefined
+  title: string | undefined
+  price: string | undefined
+  cover: string | undefined
+}
+
+async function embedOneSet() {
+  if (queue.length === 0) {
+    // Only embed again once per 24h (only happens if it takes less than 24h to embed all products)
+    if (lastStart < Date.now() - 1000 * 60 * 60 * 24) {
+      lastStart = Date.now()
+    } else {
+      return
+    }
+
+    console.log('Starting embed updates')
+
+    await products.find({}).forEach(it => {
+      if (!queue.includes(it.link)) {
+        queue.push(it.link)
+      }
+    })
+  }
+
+  const domains: Array<string> = []
+  queue = (await Promise.all(queue.map(async (link) => {
+    if (!link) return link
+    let url: URL | undefined
+    try {
+      url = new URL(link)
+    } catch (e) {
+      console.log(e)
+    }
+    if (!url) return link
+
+    if (domains.includes(url.host)) {
+      return link
+    } else {
+      domains.push(url.host)
+      console.log(` -> ${link}`)
+      const newEmbed: Embed | undefined = await (await fetch(`${isProd() ? 'https://proxy.wishlily.app' : 'http://localhost:8080'}/generic/product?id=${encodeURIComponent(link)}`)).json() as Embed | undefined
+      if (newEmbed) {
+        const { link, title, price, cover } = newEmbed
+
+        if (link) {
+          mongo.database('wishlily').collection(`products`).updateOne(
+            { link },
+            {
+              $set: {
+                link,
+                title,
+                price,
+                cover
+              }
+            },
+            { upsert: true, }
+          )
+        }
+      }
+
+      return undefined
+    }
+  }))).filter(it => it !== undefined)
+}
+
+// This stuff should run every five minutes!
+setInterval(embedOneSet, 300000)
+embedOneSet()
